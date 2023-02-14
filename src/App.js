c93bf5b60783c7a9f76557ac269dfa922c813bd9
Aleksandra <haruka.lex@gmail.com>
2023-02-14 21:01:41 +0100

    Change structure to use 'children' parameter in file structure

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import Hero from './components/Hero/Hero';
 import SearchForm from './components/SearchForm/SearchForm';
 import List from './components/List/List';
+import Container from './components/Container/Container';
 const App = ()=> {
   return (
-    <div>
+    // <div>
+    //   <Hero />
+    //   <SearchForm />
+    //   <List />
+    // </div>
+    <Container>
       <Hero />
       <SearchForm />
       <List />
-    </div>
+    </Container>
   );
 };
 export default App;
--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -2,9 +2,11 @@ import styles from './Column.module.scss';
 
 const Column = props =>{
     return (
-    <article className={styles.column}>
-        <h2 className={styles.title}>{props.title}</h2>
-    </article>
+        
+        <article className={styles.column}>
+            
+            <h3 className={styles.title}><span className={styles.icon + ' fa fa-' + props.icon} />{props.title}</h3>
+        </article>
     );
 };
 
