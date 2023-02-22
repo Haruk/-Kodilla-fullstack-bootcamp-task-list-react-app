d92de4412c7303154e9a739a815a2a4bc4da339c
Aleksandra <haruka.lex@gmail.com>
2023-02-22 23:24:45 +0100

    Add new function: remove list element

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,23 +1,31 @@
 import styles from './Card.module.scss';
 import { ButtonFavorite } from '../ButtonFavorite/ButtonFavorite';
+import { ButtonTrash } from '../ButtonTrash/ButtonTrash';
 import { useDispatch } from 'react-redux';
-import { toggleFavorite } from '../../redux/store';
+import { toggleFavorite } from '../../redux/cardsReducer';
+import { removeCard } from '../../redux/cardsReducer';
 
 const Card = (props) => {
   const dispatch = useDispatch();
 
   const handleToggleFavorite = () => {
-    // console.log(props.isFavorite);
     dispatch(toggleFavorite(props.id));
+  };
+
+  const handleRemoveCard = () => {
+    dispatch(removeCard(props.id));
   };
 
   return (
     <li className={styles.card}>
       {props.title}
-      <ButtonFavorite
-        isFavorite={props.isFavorite}
-        handleClick={handleToggleFavorite}
-      ></ButtonFavorite>
+      <div>
+        <ButtonFavorite
+          isFavorite={props.isFavorite}
+          handleClick={handleToggleFavorite}
+        />
+        <ButtonTrash handleClick={handleRemoveCard} />
+      </div>
     </li>
   );
 };
